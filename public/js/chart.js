a2258f5865aded62383f8346c3a424badbd76356
benjamin00 <[benjaminhill00@gmail.com]>
2018-03-03 13:21:42 -0600

    2nd attempt, working graph generated with static api call

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,35 +1,20 @@
 $(function () { 
    $.getJSON('https://api.coindesk.com/v1/bpi/historical/close.json?start=2010-07-17&end=2018-03-01', function(resp) {
-        //Get the returned coin data
-        var coinData = resp.bpi;
-        var dataTimePrice = [];
-        var yDataPrice = [];
+        
+        var coinData = resp.bpi;    //holds the values we're interested in from api call
 
-    for(var prop in coinData){
-        //console.log(prop + ":" + coinData[prop]);
-        //var datep = new Date(2010,06,18);
+        var dataTimePrice = [];     //holds parsed values, used for graphing
+
+
+    for(var prop in coinData){  //loops through each value, and changes the time to UTC ms
+
         var pair = {}
         pair.y = coinData[prop];
         pair.x = new Date(Date.parse(prop)).getTime()
 
-        //pair[parser(prop)] = coinData[prop]
         dataTimePrice.push(pair);
     }
-    console.log(dataTimePrice[3]);
-    console.log(dataTimePrice);
-    // var s;
-    // var myDate;
-      // for (var i = 1; i <= Object.keys(coinData).length;i++)
-      //   {
-      //       s = datep.toISOString();
-      //       s = s.substring(0,10);
-      //       myDate=s.split("-");
-      //       console.log(coinData[s]);
-      //       datep.setDate(datep.getDate() + 1);
-      //   }
-        // Create a timer
-        //var start = +new Date();
-        //console.log(coinData[0]);
+    
         var myChart = Highcharts.chart('container2', {
         chart: {
             type: 'line'
@@ -39,7 +24,7 @@ $(function () {
         },
         xAxis:{            
             type: 'datetime',
-            minRange: 3600 * 1000, // one hour
+            minRange: 3600 * 1000 * 24 * 7, // one week
             gridLineWidth: 1,
         },
         yAxis:{
@@ -47,11 +32,9 @@ $(function () {
             floor:0,
         },
         series: [{
-            turboThreshold: 0,  //Set to 0 to turn off the limit for data we can process.
+            turboThreshold: 0,
             name: "BTC",
             data: dataTimePrice,
-            // pointStart: Date.UTC(2010, 06, 18),
-            // pointInterval: 24 * 3600 * 1000, // one day
         }]
       });
 
