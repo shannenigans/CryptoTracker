73223cba1f2f7c63f828a29f570faa1f89bc6b98
benjamin00 <[benjaminhill00@gmail.com]>
2018-04-03 20:24:18 -0500

    THIS MAY NOT WORK: Added fixed Chart.js file which now also handles the drop down menu, changes the chart depending on the value of the drop down list

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,10 +1,90 @@
-$(function () { 
-
+//THE REAL ONE
+var curr = 'USD';
 var input = document.getElementsByTagName("a");
 var coin = input[0].id;
 
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");  
+};
+
+function getName(obj) {
+    var t = $(obj).text();
+    console.log(t);
+
+    if(currency_names[t]==undefined) {
+    alert(currency_symbols[currency_names]);
+    }
+    else{
+        curr = nameToAbbreviation(t);
+        generateChart(coin, curr);
+        console.log("Currency Abbreviated: " + curr);
+    };
+    document.getElementById("myDropdown").classList.toggle("show");
+    //console.log(document.getElementById("myDropdown")); 
+};
+
+var currency_symbols = {
+    'USD': '$', // US Dollar
+    'EUR': '€', // Euro
+    'CRC': '₡', // Costa Rican Colón
+    'GBP': '£', // British Pound Sterling
+    'ILS': '₪', // Israeli New Sheqel
+    'INR': '₹', // Indian Rupee
+    'JPY': '¥', // Japanese Yen
+    'KRW': '₩', // South Korean Won
+    'NGN': '₦', // Nigerian Naira
+    'PHP': '₱', // Philippine Peso
+    'PLN': 'zł', // Polish Zloty
+    'PYG': '₲', // Paraguayan Guarani
+    'THB': '฿', // Thai Baht
+    'UAH': '₴', // Ukrainian Hryvnia
+    'VND': '₫', // Vietnamese Dong
+};
+var currency_names = {
+    'US Dollars':'USD',
+    'Euros':'EUR', // Euro
+    'Costa Rican Colóns': 'CRC', // Costa Rican Colón
+    'British Pounds': 'GBP', // British Pound Sterling
+    'Israeli New Sheqels': 'ILS', // Israeli New Sheqel
+    'Indian Rupees': 'INR', // Indian Rupee
+    'Yen': 'JPY', // Japanese Yen
+    'South Korean Won':'KRW', // South Korean Won
+    'Nigerian Naira':'NGN', // Nigerian Naira
+    'Philippine Pesos': 'PHP', // Philippine Peso
+    'Polish Zloty': 'PLN', // Polish Zloty
+    'Paraguayan Guarani': 'PYG', // Paraguayan Guarani
+    'Thai Baht': 'THB', // Thai Baht
+    'Ukrainian Hryvnia': 'UAH', // Ukrainian Hryvnia
+    'Vietnamese Dong': 'VND', // Vietnamese Dong
+};
+
+//Returns the abbreviated name
+function nameToAbbreviation(obj){
+    var abbr = currency_names[obj];
+    console.log("Abbreviated version of " + obj + ": " + abbr);
+    return abbr;
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+} 
+//$(function () { 
+
+// var input = document.getElementsByTagName("a");
+// var coin = input[0].id;
+
 console.log("Coin: ", coin);
-generateChart(coin, "USD");
+generateChart(coin, curr);
 function generateChart(coin, curr) { 
         $.getJSON('https://min-api.cryptocompare.com/data/histoday?fsym=' + coin + '&tsym=' + curr + '&limit=180&aggregate=3&e=CCCAGG', function(resp) {
         
@@ -21,9 +101,9 @@ function generateChart(coin, curr) {
         chart: {
             type: 'candlestick'
         },
-        title: {
-            text: coin + ' Prices'
-        },
+        // title: {
+        //     text: coin + ' Prices'
+        // },
         navigator: {
             enabled: false
         },
@@ -58,4 +138,4 @@ function generateChart(coin, curr) {
 
     });
 }
-});
+//});
